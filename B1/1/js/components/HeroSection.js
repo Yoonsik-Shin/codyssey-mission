35bc1592e0583daf19a63a6c6df8a5ba7e8f9727
Yoonsik-Shin <sys25800994@gmail.com>
2026-09-18 10:55:52 +0900

    refactor: apply ES private fields and methods to HeroSection

--- B1/1/js/components/HeroSection.js
+++ B1/1/js/components/HeroSection.js
@@ -11,55 +11,54 @@ export class HeroSection extends BaseComponent {
     return BaseComponent.resolveCss("HeroSection.css");
   }
 
-  constructor() {
-    super();
-    this.phrases = [
-      "안녕하세요! 개발자 신윤식입니다. 👋",
-      "사용자 경험과 깔끔한 코드를 고민합니다. 🚀",
-      "웹 컴포넌트로 만드는 모던 웹 애플리케이션. 💡",
-    ];
-    this.phraseIndex = 0;
-    this.charIndex = 0;
-    this.isDeleting = false;
-    this.timer = null;
-  }
+  // 🔒 Private 필드 선언
+  #phrases = [
+    "안녕하세요! 개발자 신윤식입니다. 👋",
+    "사용자 경험과 깔끔한 코드를 고민합니다. 🚀",
+    "웹 컴포넌트로 만드는 모던 웹 애플리케이션. 💡",
+  ];
+  #phraseIndex = 0;
+  #charIndex = 0;
+  #isDeleting = false;
+  #timer = null;
 
   mounted() {
-    this.runTypewriterEffect();
+    this.#runTypewriterEffect();
   }
 
   unmounted() {
-    if (this.timer) {
-      clearTimeout(this.timer);
+    if (this.#timer) {
+      clearTimeout(this.#timer);
     }
   }
 
-  runTypewriterEffect() {
+  // 타자기 효과 실행
+  #runTypewriterEffect() {
     const textEl = this.shadowRoot.querySelector(".typewriter-text");
     if (!textEl) return;
 
-    const currentPhrase = this.phrases[this.phraseIndex];
+    const currentPhrase = this.#phrases[this.#phraseIndex];
 
-    if (this.isDeleting) {
-      this.charIndex--;
+    if (this.#isDeleting) {
+      this.#charIndex--;
     } else {
-      this.charIndex++;
+      this.#charIndex++;
     }
 
-    textEl.textContent = currentPhrase.substring(0, this.charIndex);
+    textEl.textContent = currentPhrase.substring(0, this.#charIndex);
 
-    let speed = this.isDeleting ? 40 : 80;
+    let speed = this.#isDeleting ? 40 : 80;
 
-    if (!this.isDeleting && this.charIndex === currentPhrase.length) {
+    if (!this.#isDeleting && this.#charIndex === currentPhrase.length) {
       speed = 1800; // 한 문장 완료 후 대기
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.charIndex === 0) {
-      this.isDeleting = false;
-      this.phraseIndex = (this.phraseIndex + 1) % this.phrases.length;
+      this.#isDeleting = true;
+    } else if (this.#isDeleting && this.#charIndex === 0) {
+      this.#isDeleting = false;
+      this.#phraseIndex = (this.#phraseIndex + 1) % this.#phrases.length;
       speed = 400;
     }
 
-    this.timer = setTimeout(() => this.runTypewriterEffect(), speed);
+    this.#timer = setTimeout(() => this.#runTypewriterEffect(), speed);
   }
 
   setEvents() {
